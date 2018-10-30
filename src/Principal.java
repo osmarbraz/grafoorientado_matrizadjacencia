c5f07768d9cd7dfaa6afade681f58e1ae57c48ad
osmar <osmar@suporte-PC>
2018-10-30 12:10:10 -0300

    Correção de métodos e comentários

--- src/Principal.java
+++ src/Principal.java
@@ -15,14 +15,14 @@ public class Principal {
 
     /**
      * Troca um número pelo rótulo do vértice no grafo.
-     * 
+     *
      * Retorna o rótulo da posição i do vértice no grafo.
      *
      * @param i Posição do vértice no grafo
      * @return Um rótulo para a posição i
      */
-    public static String rotuloVertice(int i) {        
-        String [] rotulos = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    public static String rotuloVertice(int i) {
+        String[] rotulos = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         if ((i >= 0) && (i <= rotulos.length)) {
             return (rotulos[i] + "");
         } else {
@@ -36,8 +36,8 @@ public class Principal {
      * @param chave Rótulo a ser trocado pela posição do vértice no grafo.
      * @return Um inteiro que representa a posição do vértice no grafo
      */
-    public static int indiceRotuloVertice(String chave) {        
-        String [] rotulos = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    public static int indiceRotuloVertice(String chave) {
+        String[] rotulos = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         int pos = -1;
         for (int i = 0; i < rotulos.length; i++) {
             if (rotulos[i].equals(chave)) {
@@ -91,6 +91,7 @@ public class Principal {
             {0, 0, 0, 0, 0, 0, 0},//6
             {0, 0, 0, 0, 1, 0, 0} //7
         };
+
 //        int[][] g = {
 //            //1  2  3 
 //            {0, 1, 1},//1
@@ -108,31 +109,32 @@ public class Principal {
 
     /**
      * Verifica se existe a aresta no Grafo.
+     *
      * @param G Grafo a ser verifica a aresta.
      * @param i Vértice adjacente i da aresta.
      * @param j Vértice adjacente j da aresta.
      * @return Se existe a aresta;
      */
-    public static boolean existeAresta(int[][] G, int i, int j){
-        if (G[i][j]==1){
+    public static boolean existeAresta(int[][] G, int i, int j) {
+        if (G[i][j] == 1) {
             return true;
         } else {
             return false;
         }
     }
-    
+
     /**
      * Incluir um areta para o Grafo.
-     * 
+     *
      * @param G Gravo a ser inserido a aresta.
      * @param i Vértice adjacente.
      * @param j Vértice adjacente.
      */
-    public static void incluirAresta(int[][] G, int i, int j){
+    public static void incluirAresta(int[][] G, int i, int j) {
         //1 valor para a aresta que possui o vértice i e j como adjacentes.
-         G[i][j] = 1;
+        G[i][j] = 1;
     }
-    
+
     /**
      * Realiza a leitura dos dados do Grafo G.
      */
@@ -146,12 +148,48 @@ public class Principal {
                     + "\nDigite o indice(0-" + n + ") do vértice de incidência de " + i
                     + "\n ou -1 para ir ao próximo vértice:"));
             while (j != -1) {
-               incluirAresta(G,i,j);
+                incluirAresta(G, i, j);
                 j = Integer.parseInt(JOptionPane.showInputDialog("Preenchendo os adjacentes de(" + rotuloVertice(i) + ")"
                         + "\nDigite o índice(0-" + n + ") do vértice de incidência de " + i
                         + "\n ou -1 para ir ao próximo vértice:"));
             }
         }
+    }
+
+    /**
+     * Retorna a quantidade de vértices do Grafo.
+     *
+     * @param G Matriz do grafo.
+     * @param n Quantidade de vértices do grafo.
+     * @return A quantidade vértices do grafo.
+     */
+    public static int quantidadeVertices(int[][] G, int n) {
+        //A quantidade de vértices é o tamanho da matriz.
+        return n;
+    }
+
+    /**
+     * Retorna a quantidade de arestas do Grafo.
+     *
+     * Realiza a soma de todas as posições A[i][j] do grafo.
+     *
+     * @param G Matriz do grafo.
+     * @param n Quantidade de vértices do grafo.
+     * @return A quantidade de arestas do grafo.
+     */
+    public static int quantidadeArestas(int[][] G, int n) {
+        //Acumula a quantidade de arestas
+        int conta = 0;
+        //Percorre a matriz de adjacência
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //Verifica se´é diferente de 0 ou seja existe aresta em G[i][j].                 
+                if (G[i][j] != 0) {
+                    conta = conta + G[i][j];
+                }
+            }
+        }
+        return conta;
     }
 
     /**
@@ -181,10 +219,12 @@ public class Principal {
         int conta = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                //Percorre toda matriz para encontrar 1
-                if (G[i][j] == 1) {
-                    saida = saida + "e" + conta + ",";
-                    conta = conta + 1;
+                //Verifica se´é diferente de 0 ou seja existe aresta em G[i][j]. 
+                if (G[i][j] != 0) {
+                    for (int x = 0; x < G[i][j]; x++) {
+                        saida = saida + "e" + conta + ",";
+                        conta = conta + 1;
+                    }
                 }
             }
         }
@@ -203,10 +243,13 @@ public class Principal {
         int conta = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                //Verifica se existe par para i e j
-                if (G[i][j] == 1) {
-                    saida = saida + "e" + conta + "=(" + rotuloVertice(i) + "," + rotuloVertice(j) + ") \n";
-                    conta = conta + 1;
+                //Verifica se´é diferente de 0 ou seja existe aresta em G[i][j]. 
+                if (G[i][j] != 0) {
+                    //Adiciona uma aresta para ocorrência
+                    for (int x = 0; x < G[i][j]; x++) {
+                        saida = saida + "e" + conta + "=(" + rotuloVertice(i) + "," + rotuloVertice(j) + "),";
+                        conta = conta + 1;
+                    }
                 }
             }
         }
@@ -229,10 +272,10 @@ public class Principal {
         //Os vértices x e y devem ser menor que n
         if ((i < n) && (j < n)) {
             //Verifica se é diferente de 0 ou seja existe aresta.
-            if (G[i][j] == 0) {
-                return false;
-            } else {
+            if (G[i][j] != 0) {
                 return true;
+            } else {
+                return false;
             }
         } else {
             return false;
@@ -249,11 +292,11 @@ public class Principal {
      */
     public static int grauSaidaVertice(int[][] G, int n, int i) {
         int conta = 0;
-        //Percorre a linha de adjacencia de j
+        //Percorre a linha de adjacencia de i
         for (int j = 0; j < n; j++) {
-            //Verifica se existe adjacente
-            if (G[i][j] == 1) {
-                conta = conta + 1;
+            //Verifica se é diferente de 0 ou seja existe aresta em G[i][j]
+            if (G[i][j] != 0) {
+                conta = conta + G[i][j];
             }
         }
         return conta;
@@ -292,8 +335,9 @@ public class Principal {
         //Percorre a coluna de adjacencia de j
         for (int i = 0; i < n; i++) {
             //Verifica se existe adjacente
-            if (G[i][j] == 1) {
-                conta = conta + 1;
+            //Verifica se é diferente de 0 ou seja existe aresta em G[i][j]
+            if (G[i][j] != 0) {
+                conta = conta + G[i][j];
             }
         }
         return conta;
@@ -465,16 +509,29 @@ public class Principal {
      * @return Se o grafo é completo.
      */
     public static boolean eCompleto(int[][] G, int n) {
+        boolean diagonal0 = true;
+        //Verifica se a diagonal principal não possui laços
+        for (int i = 0; i < n; i++) {
+            if (G[i][i] != 0) {
+                diagonal0 = false;
+            }
+        }
+        
+        boolean acima1 = true;
+        //Verifica se fora da diagonal principal existes arestas simples(1)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                //Para todo vértice diferente da diagonal principal(i!=j) existe
-                //uma aresta(diferente de 0)
-                if ((G[i][j] == 0) && (i != j)) {
-                    return false;
+                if (G[i][j] != 1) {
+                    acima1 = false;
                 }
             }
         }
-        return true;
+        //Verifica as duas condições
+        if ((diagonal0 == true) && (acima1 == true)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -578,6 +635,51 @@ public class Principal {
     }
 
     /**
+     * Converte a representação de matriz de Adjacência para matriz de
+     * Incidência.
+     *
+     * @param G Matriz do grafo.
+     * @param n Quantidade de vértices do grafo.
+     * @return Uma matriz de incidência.
+     */
+    public static int[][] converte(int[][] G, int n) {
+        //Conta o número de arestas
+        int contaArestas = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //Somente diferente de 0 e que fazem parte do 
+                //Triângulo superior 
+                if (G[i][j] != 0) {
+                    //Conta o número de arestas 
+                    contaArestas = contaArestas + G[i][j];
+                }
+            }
+        }
+
+        //Cria a matriz de incidência
+        int[][] I = new int[n][contaArestas];
+
+        //Conta o número de arestas
+        int e = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //Somente diferente de 0 e que fazem parte do 
+                //Triângulo superior
+                if (G[i][j] != 0) {
+                    //Adiciona uma incidência para cada aresta
+                    for (int x = 0; x < G[i][j]; x++) {
+                        I[i][e] = 1;
+                        I[j][e] = 1;
+                        //Avança para a próxima aresta
+                        e = e + 1;
+                    }
+                }
+            }
+        }
+        return I;
+    }
+
+    /**
      * Programa principal.
      *
      * @param args
@@ -611,6 +713,7 @@ public class Principal {
                     + "19- Verifica se o grafo é regular\n"
                     + "20- Verifica se o grafo possui ciclo Euleriano\n"
                     + "21- Verifica se o grafo possui caminho Euleriano\n"
+                    + "22- Converte para Matriz de Incidência\n"
                     //Outras opções vão aqui                    
                     + "99- Sair\n"
                     + "Digite a opção desejada:"));
@@ -846,6 +949,20 @@ public class Principal {
                     JOptionPane.showMessageDialog(null, saida);
                     break;
                 }
+
+                case 22: {
+                    int[][] I = converte(G, n);
+                    int e = quantidadeArestas(G, n);
+
+                    //Recupera os dados da matriz
+                    String dados = "Matriz Incidência:" + "\n" + imprimirMatriz(I, n, e);
+                    //Adiciona a String em um TextArea
+                    JTextArea saida = new JTextArea(dados);
+                    //Exibe o TextArea com showMessageDialog
+                    JOptionPane.showMessageDialog(null, saida);
+                    break;
+                }
+
                 //Opção de saída do programa
                 case 99: {
                     System.out.println("Saindo do programa!");
